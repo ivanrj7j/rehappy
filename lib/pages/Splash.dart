9b67d1f4968d2ffcf7de7b3c265f00effddfd6a0
Ameesha005 <a.m.e.e.s.h.a.p.1237@gmail.com>
2025-01-18 21:38:18 +0530

    added splash

--- lib/pages/Splash.dart
+++ lib/pages/Splash.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
+
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 412,
-          height: 917,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(color: Color(0xFF232323)),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 412,
-                height: 183,
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xFF232323)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch, // Use stretch to fill the width
+          children: [
+            Expanded(
+              child: Container(
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     image: NetworkImage("https://via.placeholder.com/412x183"),
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
