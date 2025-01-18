5beebd997ee4831c7eaab7652e077200ba2de5bb
Ameesha005 <a.m.e.e.s.h.a.p.1237@gmail.com>
2025-01-18 21:51:41 +0530

    added create community

--- lib/pages/craeteCommunity.dart
+++ lib/pages/craeteCommunity.dart
@@ -3,288 +3,193 @@ import 'package:flutter/material.dart';
 class CreateCommunity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 412,
-          height: 917,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment(0.00, -1.00),
-              end: Alignment(0, 1),
-              colors: [Color(0xFF191919), Color(0xFF222222)],
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF191919), Color(0xFF222222)],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildHeaderIcons(context),
+                _buildCommunityPictureSection(context),
+                _buildCommunityNameInput(context),
+                _buildCommunityDescriptionInput(context),
+                SizedBox(height: 20), // Added some padding at the bottom
+              ],
             ),
           ),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 106,
-                top: 50,
-                child: Container(
-                  width: 212,
-                  height: 241,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 200,
-                          height: 200,
-                          decoration: ShapeDecoration(
-                            color: Color(0xFFD9D9D9),
-                            shape: OvalBorder(),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 150,
-                        top: 150,
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: ShapeDecoration(
-                                    gradient: LinearGradient(
-                                      begin: Alignment(0.00, -1.00),
-                                      end: Alignment(0, 1),
-                                      colors: [
-                                        Color(0xFF1B1B1B),
-                                        Color(0xFF1B1B1B)
-                                      ],
-                                    ),
-                                    shape: OvalBorder(
-                                      side: BorderSide(
-                                          width: 1, color: Color(0xFF424549)),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                left: 12.50,
-                                top: 12.50,
-                                child: Container(
-                                  width: 25,
-                                  height: 25,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                          "https://via.placeholder.com/25x25"),
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 4,
-                        top: 210,
-                        child: Text(
-                          'Set Community Picture',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: 'Kdam Thmor Pro',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildHeaderIcons(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Transform(
+          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.14),
+          child: Container(
+            width: 33,
+            height: 33,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://via.placeholder.com/33x33"),
+                fit: BoxFit.fill,
               ),
-              Positioned(
-                left: 0,
-                top: 366,
-                child: Container(
-                  width: 412,
-                  height: 76,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 31,
-                        child: Container(
-                          width: 412,
-                          height: 45,
-                          padding: const EdgeInsets.all(10),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 368,
-                                child: Text(
-                                  'Enter community name',
-                                  style: TextStyle(
-                                    color: Color(0xFFA6A9AD),
-                                    fontSize: 16,
-                                    fontFamily: 'Kdam Thmor Pro',
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 10,
-                        top: 0,
-                        child: Text(
-                          'Community Name',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: 'Kdam Thmor Pro',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 386,
-                        top: 5,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://via.placeholder.com/20x20"),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+            ),
+          ),
+        ),
+        Transform(
+          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.14),
+          child: Container(
+            width: 33,
+            height: 33,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://via.placeholder.com/33x33"),
+                fit: BoxFit.fill,
               ),
-              Positioned(
-                left: 0,
-                top: 517,
-                child: Container(
-                  width: 412,
-                  height: 76,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 31,
-                        child: Container(
-                          width: 412,
-                          height: 45,
-                          padding: const EdgeInsets.all(10),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 368,
-                                child: Text(
-                                  'Enter description about the community',
-                                  style: TextStyle(
-                                    color: Color(0xFFA6A9AD),
-                                    fontSize: 16,
-                                    fontFamily: 'Kdam Thmor Pro',
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 10,
-                        top: 0,
-                        child: Text(
-                          'Community Description ',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: 'Kdam Thmor Pro',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 386,
-                        top: 5,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://via.placeholder.com/20x20"),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 55,
-                top: 50,
-                child: Transform(
-                  transform: Matrix4.identity()
-                    ..translate(0.0, 0.0)
-                    ..rotateZ(3.14),
-                  child: Container(
-                    width: 33,
-                    height: 33,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image:
-                            NetworkImage("https://via.placeholder.com/33x33"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 390,
-                top: 50,
-                child: Transform(
-                  transform: Matrix4.identity()
-                    ..translate(0.0, 0.0)
-                    ..rotateZ(3.14),
-                  child: Container(
-                    width: 33,
-                    height: 33,
-                    decoration: ShapeDecoration(
-                      image: DecorationImage(
-                        image:
-                            NetworkImage("https://via.placeholder.com/33x33"),
-                        fit: BoxFit.fill,
-                      ),
-                      shape: OvalBorder(),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              shape: BoxShape.circle,
+            ),
           ),
         ),
       ],
+    );
+  }
+
+
+  Widget _buildCommunityPictureSection(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Color(0xFFD9D9D9),
+              shape: BoxShape.circle,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Set Community Picture',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'Kdam Thmor Pro',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 10),
+          GestureDetector(
+            onTap: (){
+              // Handle community image selection action
+            },
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xFF1B1B1B), Color(0xFF1B1B1B)],
+                  ),
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Color(0xFF424549),
+                  )
+              ),
+              child: Center(
+                child: Container(
+                  width: 25,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage("https://via.placeholder.com/25x25"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCommunityNameInput(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Community Name',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'Kdam Thmor Pro',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 5),
+          TextField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+                hintText: 'Enter community name',
+                hintStyle: TextStyle(color: Color(0xFFA6A9AD)),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade700),
+                  borderRadius: BorderRadius.circular(5),
+                )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCommunityDescriptionInput(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Community Description ',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'Kdam Thmor Pro',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 5),
+          TextField(
+              maxLines: 3,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  hintText: 'Enter description about the community',
+                  hintStyle: TextStyle(color: Color(0xFFA6A9AD)),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade700),
+                    borderRadius: BorderRadius.circular(5),
+                  )
+              )
+          ),
+        ],
+      ),
     );
   }
 }
